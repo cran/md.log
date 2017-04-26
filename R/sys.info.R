186df24af2c9af530d61db8b12fe36ef346e64dc
E. F. Haghish <e.f.haghish@uni-greifswald.de>
2017-04-26 16:27:01 +0000

    version 0.1.1

---
+++ R/sys.info.R
@@ -0,0 +1,25 @@
+sys.info <- function() {
+  sysinf <- Sys.info()
+  os <- sysinf['sysname']
+  release = sysinf['release']
+  cpu = sysinf['machine']
+
+  # Setting for Mac OS X
+  if (!is.null(sysinf)){
+    if (os == 'Darwin') {
+      str = unlist(strsplit(sessionInfo()$running, split = " "))
+      os = paste("Mac OSX", str[2])
+      release = str[3]
+      #release = as.numeric_version(system("sw_vers -productVersion", intern = TRUE))
+    }
+  }
+
+  else { ## mystery machine
+    os <- .Platform$OS.type
+    if (grepl("^darwin", R.version$os))
+      os <- "Mac OS X"
+    if (grepl("linux-gnu", R.version$os))
+      os <- "Linux"
+  }
+  return(list(os=os, release=release, cpu=cpu))
+}
